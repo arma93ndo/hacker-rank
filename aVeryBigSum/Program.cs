22356c9d50739017b3adf0227eb9696f995d07ab
Alex Ruiz <arma93ndo@.com>
2026-01-30 21:31:30 -0600

    Complete "A Very Big Sum" challenge

---
+++ aVeryBigSum/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+// Input
+List<long> ar = new List<long> { 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 };
+
+/////////////////////////////
+
+long sum = 0;
+for (int i = 0; i < ar.Count; i++)
+    sum += ar[i];
+
+
+Console.WriteLine(sum);
+// return sum;
