05c08733cb716e3896c416334462ea08376a9fbe
Alex Ruiz <arma93ndo@.com>
2026-01-30 22:55:14 -0600

    Complete "Staircase" challenge

---
+++ staircase/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Input
+
+
+int n = 6;
+////////////////////////////////
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (n - (i + 1) > j)
+        {
+            Console.Write(" ");
+
+        }
+        else
+        {
+            Console.Write("#");
+        }
+    }
+    Console.WriteLine();
+}
+
+
