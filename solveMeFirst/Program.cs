33e33a9d06be8df6b05a9d3a50de8ff878a50863
Alex Ruiz <arma93ndo@.com>
2026-01-30 20:45:55 -0600

    Add a gitignore file

---
+++ solveMeFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Input
+int a, b;
+a = 2;
+b = 3;
+////////////////////////////////////////////////////////////////
+
+
+Console.WriteLine(a + b); // return a + b;
+
+
