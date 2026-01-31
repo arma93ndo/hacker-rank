add5a6c2874834a67b28a32abcc269c2a2be616c
Alex Ruiz <arma93ndo@.com>
2026-01-30 22:09:07 -0600

    Complete "Plus Minus" Challenge

---
+++ plusMinus/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// Input
+
+List<int> arr = new List<int> { -4, 3, -9, 0, 4, 1 };
+///////////////////////////////////////////
+
+int positive = 0;
+int negative = 0;
+int zeroes = 0;
+
+for (int i = 0; i < arr.Count; i++)
+{
+    if (arr[i] > 0)
+        positive++;
+    else if (arr[i] < 0)
+        negative++;
+    else if (arr[i] == 0)
+        zeroes++;
+}
+
+Console.WriteLine("{0:0.######}", ((float)positive / arr.Count));
+Console.WriteLine("{0:0.######}", (float)negative / arr.Count);
+Console.WriteLine("{0:0.######}", (float)zeroes / arr.Count);
+
