873b5af7659f515a42cd7256d6e7b502226f05e9
Alex Ruiz <arma93ndo@.com>
2026-02-01 17:31:51 -0600

    Complete "Time Conversion" challenge

---
+++ timeConversion/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Input:
+
+
+string s = "12:05:45PM";
+//////////////////////////
+
+string[] strings = s.Split(':');
+
+string hour = strings[0];
+string minutes = strings[1];
+string seconds = strings[2];
+string p = seconds[2].ToString();
+
+seconds = seconds.Remove(2);
+
+if (p == "P")
+{
+    hour = (int.Parse(hour) + 12).ToString();
+}
+
+if (hour == "12" && p == "A")
+{
+    hour = "00";
+}
+
+if (hour == "24" && p == "P")
+{
+    hour = "12";
+}
+
+Console.WriteLine(hour + ":" + minutes + ":" + seconds);
