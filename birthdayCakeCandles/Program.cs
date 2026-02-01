be5effe72fcef604cc9cc4b1f741cf37cab7e8ca
Alex Ruiz <arma93ndo@.com>
2026-02-01 17:04:26 -0600

    Complete "Birthday Cake Candles" challenge

---
+++ birthdayCakeCandles/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Input
+using System.Collections.Generic;
+
+
+List<int> candles = new List<int> { 3, 2, 1, 3 };
+////////////////////////
+
+// Solution A:
+// int max = candles.Max();
+// int count = candles.Count(c => c == max);
+// Console.WriteLine(count);
+
+// Solution B:
+int max = 0;
+int count = 0;
+
+foreach (int candle in candles)
+{
+    if (candle > max)
+    {
+        max = candle;
+        count = 1;
+    }
+    else if (max == candle)
+        count++;
+}
+
+Console.WriteLine(count);
+
