bd2d7b30e57311e54e68b042ce51a1c4980e990c
Alex Ruiz <arma93ndo@.com>
2026-02-01 16:42:57 -0600

    Complete "Simple Array Sum" challenge

---
+++ simpleArraySum/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+
+
+// Input
+
+List<int> ar = new List<int> { 1, 2, 3, 4, 10, 11 };
+////////////////////////
+
+int sum = 0;
+for (int i = 0; i < ar.Count; i++)
+{
+    sum += ar[i];
+}
+
+Console.WriteLine(sum);
