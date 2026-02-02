d68bc93e36a16a41f900ec4dd18f4e317a16d938
Alex Ruiz <arma93ndo@.com>
2026-02-01 21:59:35 -0600

    Complete "Number Line Jumps" challenge

---
+++ numberLineJumps/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Input:
+
+int x1 = 0, v1 = 2,  x2 = 5,  v2 = 3;
+///////////////////////////////////////
+
+int kangaroo1 = x1;
+int kangarro2 = x2;
+for (int i = 0; i < 10000; i++)
+{
+    if(kangaroo1 + (i * v1) == kangarro2 + (i * v2))
+    {
+        Console.WriteLine("YES");
+    }
+    
+}
+
+Console.WriteLine( "NO");
+
