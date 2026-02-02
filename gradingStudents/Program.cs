9316c846c0be0d1c20da968aec2f4c0e5a9137cd
Alex Ruiz <arma93ndo@.com>
2026-02-01 18:07:41 -0600

    Complete "Grading Students" challenge

---
+++ gradingStudents/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Input:
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Numerics;
+
+
+List<int> grades = new List<int> {
+    73, 67, 38, 33
+};
+////////////////////////
+
+
+for (int i = 0; i < grades.Count; i++)
+{
+    if (grades[i] < 38) continue;
+
+    int nextMultiple = grades[i] + 1;
+
+    while (nextMultiple % 5 != 0)
+        nextMultiple++;
+
+    if (nextMultiple - grades[i] < 3)
+        grades[i] = nextMultiple;
+
+}
+
+
+foreach (int grade in grades)
+{
+    Console.WriteLine(grade);
+}
