f9850a0bcd977698ed51e53749daeeb9e9aea670
Alex Ruiz <arma93ndo@.com>
2026-02-02 18:46:31 -0600

    Complete "Divisible Sum Pairs" challenge

---
+++ divisibleSumPairs/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Input:
+
+int n = 6; 
+int k = 3;
+List<int> ar = new List<int> {1, 3, 2, 6, 1, 2};
+
+///////////////////////////////
+
+int pairs = 0;
+
+for(int i = 0; i < ar.Count; i++)
+{
+    for(int j = i + 1; j < ar.Count; j++)
+    {
+        if ((ar[i] + ar[j]) % k == 0) pairs++;
+    }
+}
+
+Console.WriteLine(pairs);
+
+
