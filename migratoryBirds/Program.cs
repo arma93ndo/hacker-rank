2e56b15ac3b438b457033bbd21d067cff4d9f721
Alex Ruiz <arma93ndo@.com>
2026-02-02 19:17:31 -0600

    Complete "Migratory Birds" challenge

---
+++ migratoryBirds/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Input:
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+List<int> arr = new List<int> {1, 4, 4, 4, 5, 3};
+//////////////////////////////////
+
+int[] types = new int[6];
+
+for (int i = 0; i < arr.Count; i++)
+{
+    types[arr[i]]++;
+}
+
+int max = int.MinValue;
+int mode = 0;
+for(int i = 0; i < types.Length; i++)
+{
+    if (types[i] > max)
+    {
+        max = types[i];
+        mode = i;    
+    }
+}
+
+Console.WriteLine(mode);
